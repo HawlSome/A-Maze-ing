1712298ed3176c1268d1bfdfe4f7263b28392afd
varandri <varandri@student.42antananarivo.mg>
2026-05-01 14:31:13 +0300

    classes: Done

--- classes/cell.py
+++ classes/cell.py
@@ -6,14 +6,57 @@
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/01 11:00:59 by varandri            #+#    #+#            #
-#   Updated: 2026/05/01 11:03:19 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/01 14:00:46 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 class Cell:
-    def __init__(self) -> None:
-        self.coordination: tuple[int, int]
-        self.north = 1
-        self.south = 1
-        self.west = 1
-        self.east = 1
+    def __init__(self, coordinate: tuple[int, int] = (0, 0)) -> None:
+        self._coordinate: tuple[int, int] = coordinate
+        self._north: int = 1
+        self._south: int = 1
+        self._west: int = 1
+        self._east: int = 1
+        self._isolation: bool = False
+
+    def get_coordinate(self) -> tuple[int, int]:
+        return self._coordinate
+
+    def open_wall(self, wall: str = "") -> None:
+        wall = wall.lower()
+        if self._isolation:
+            return
+        if wall == "west":
+            self._west = 0
+        elif wall == "south":
+            self._south = 0
+        elif wall == "east":
+            self._east = 0
+        elif wall == "north":
+            self._north = 0
+        else:
+            return
+
+    def close_wall(self, wall: str = "") -> None:
+        wall = wall.lower()
+        if self._isolation:
+            return
+        if wall == "west":
+            self._west = 1
+        elif wall == "south":
+            self._south = 1
+        elif wall == "east":
+            self._east = 1
+        elif wall == "north":
+            self._north = 1
+        else:
+            return
+
+    def set_isolation(self) -> None:
+        self._isolation = True
+
+    def get_hex(self) -> str:
+        bits: list[int] = [self._west, self._south, self._east, self._north]
+        int_value: int = int("".join(map(str, bits)), 2)
+        hex_value: str = hex(int_value)
+        return hex_value[2:]
