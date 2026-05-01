99e52619c700d98360e8ece0073bb77d4cf5760c
varandri <varandri@student.42antananarivo.mg>
2026-05-01 15:16:45 +0300

    a_maze_ing.py: Done

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -6,7 +6,7 @@
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/04/29 17:00:15 by varandri            #+#    #+#            #
-#   Updated: 2026/05/01 13:56:27 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/01 15:16:34 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -16,4 +16,5 @@ if __name__ == "__main__":
     cell = Cell((0, 1))
     cell.open_wall("south")
     cell.open_wall("west")
+    cell.open_wall("east")
     print(cell.get_hex())
