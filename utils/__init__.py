32517602fde11ad3364035802b8a2b9a89b516ff
varandri <varandri@student.42antananarivo.mg>
2026-05-02 14:04:33 +0300

    utils: Done

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,15 +6,17 @@
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/04/29 17:07:28 by varandri            #+#    #+#            #
-#   Updated: 2026/05/01 16:51:39 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/02 13:12:34 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 from .utils_cells import generate_cells, get_neighbors, connect_cells
 from .utils_cells_walls import open_east, open_north, open_south, open_west
+from .read_file import read_file
 
 
 __all__ = [
     "generate_cells", "get_neighbors", "connect_cells",
-    "open_east", "open_north", "open_south", "open_west" 
+    "open_east", "open_north", "open_south", "open_west",
+    "read_file"
 ]
--- utils/read_file.py
+++ utils/read_file.py
@@ -6,7 +6,7 @@
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/01 15:56:12 by varandri            #+#    #+#            #
-#   Updated: 2026/05/02 11:55:29 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/02 13:35:26 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -39,24 +39,27 @@ def parse_value(value: str) -> Any:
 
 def read_file(file: "str") -> dict[str, Any]:
     config: dict[str, Any] = {}
-    with open(file, "r") as f:
-        try:
+    try:
+        with open(file, "r") as f:
             for line in f:
                 if "#" in line:
                     line = line[:line.index("#")].strip()
                 if not line:
                     continue
                 if "=" not in line:
-                    raise Exception
+                    raise Exception("Key with no assigned value")
                 key, value = line.split("=", 1)
                 if "=" in value or not value.strip():
-                    raise Exception
+                    raise Exception("Key with no value found")
                 config[key.lower().strip()] = parse_value(value.strip())
             if not validate_config(config):
-                raise Exception
-        except Exception:
-            print("Error")
+                raise Exception("Invalid file/value format")
+    except (Exception, FileNotFoundError) as e:
+        if isinstance(e, FileNotFoundError):
+            print("Error: File not found")
             sys.exit()
+        print(f"Error: {e}")
+        sys.exit()
     return config
 
 
@@ -66,14 +69,17 @@ def validate_config(config: dict[str, Any]) -> bool:
         "exit", "output_file", "perfect"
     ]
     for key in mandatory:
-        if key not in list(config.keys()):
+        if key not in config:
             return False
     for key, value in config.items():
-        if key == "width" or key == "height" and not isinstance(int, value):
+        if key in ("width", "height") and not isinstance(value, int):
             return False
-        elif (key == "entry" or key == "exit" and
-              not isinstance(tuple[int, int], value)):
+        elif key in ("entry", "exit") and not (
+            isinstance(value, tuple) and
+            len(value) == 2 and
+            all(isinstance(part, int) for part in value)
+        ):
             return False
-        elif (key == "perfect" and not isinstance(bool, value)):
+        elif (key == "perfect" and not isinstance(value, bool)):
             return False
     return True
