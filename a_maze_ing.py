91b4d8b2bb8e9f4ee6b49be9f957b824505add95
varandri <varandri@student.42antananarivo.mg>
2026-05-02 14:04:25 +0300

    a_maze_ing.py: Done

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -6,16 +6,21 @@
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/04/29 17:00:15 by varandri            #+#    #+#            #
-#   Updated: 2026/05/01 15:20:34 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/02 13:12:43 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
-from classes import Cell
-from utils import connect_cells
+# from classes import Cell
+# from utils import connect_cells
+from utils import read_file
+from packages import sys
 
 if __name__ == "__main__":
-    cell = Cell(1, 0)
-    cell2 = Cell(1, 1)
-    connect_cells(cell, cell2)
-    print(cell.get_hex())
-    print(cell2.get_hex())
+    # cell = Cell(1, 0)
+    # cell2 = Cell(1, 1)
+    # connect_cells(cell, cell2)
+    # print(cell.get_hex())
+    # print(cell2.get_hex())
+    _, file = sys.argv
+    test = read_file(file)
+    print(test)
