84489d9ecb74af56ebe67e55d51e136cff53ba96
Nomenaaa <nrasolomampianina@gmail.com>
2026-05-10 16:04:14 +0200

    feat: add accessible neighbor in utils_cells

--- mazegenerator/utils/utils_cells.py
+++ mazegenerator/utils/utils_cells.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
 # ########################################################################### #
-#                                                                             #
+#   shebang: 1                                                                #
 #                                                          :::      ::::::::  #
 #   utils_cells.py                                       :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
-#   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
+#   By: nrasolom <nrasolom@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/01 15:56:09 by varandri            #+#    #+#            #
-#   Updated: 2026/05/09 16:02:47 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/10 16:00:37 by nrasolom           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
-from ..classes import Cell
+from ..classes import Cell, Directions
 from .utils_cells_walls import (
     open_west, open_east, open_north, open_south,
     close_west, close_east, close_north, close_south
@@ -58,6 +59,26 @@ def get_neighbors(
         not cells[y + 1][x].get_visit()
     ):
         neighbors.append((current, cells[y + 1][x]))
+    return neighbors
+
+
+def get_accessible_neighbors(
+        current: Cell, cells: list[list[Cell]]
+) -> list[Cell]:
+    neighbors: list[Cell] = []
+    width: int = len(cells[0]) - 1
+    height: int = len(cells) - 1
+    x, y = current.get_coordinate()
+
+    if x > 0 and not current.has_wall(Directions.W):
+        neighbors.append(cells[y][x - 1])
+    if x < width and not current.has_wall(Directions.E):
+        neighbors.append(cells[y][x + 1])
+    if y > 0 and not current.has_wall(Directions.N):
+        neighbors.append(cells[y - 1][x])
+    if y < height and not current.has_wall(Directions.S):
+        neighbors.append(cells[y + 1][x])
+
     return neighbors
 
 
