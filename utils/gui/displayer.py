60d7c1d9ffed55816f8eb39905ec4757e840e6b3
varandri <varandri@student.42antananarivo.mg>
2026-05-24 11:05:50 +0300

    mlx_initilized

--- utils/gui/displayer.py
+++ utils/gui/displayer.py
@@ -6,7 +6,13 @@
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/24 10:58:25 by varandri            #+#    #+#            #
-#   Updated: 2026/05/24 10:58:27 by varandri           ###   ########.fr      #
+#   Updated: 2026/05/24 11:05:25 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
+
+class MazeDisplayer:
+    def __init__(
+        self, cell_size: int, wall_size: int, win_width: int, win_height: int
+    ) -> None:
+        
