059bad2dec4986f7ba994e3b920582232e4fd21b
Hawmee <nomenaloicrandriann@gmail.com>
2026-06-13 00:11:06 +0300

    improvements: Docstrings - added

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,35 +1,22 @@
 #!/usr/bin/env python3
 # ########################################################################### #
-#                                                                             #
+#   shebang: 1                                                                #
 #                                                          :::      ::::::::  #
 #   a_maze_ing.py                                        :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/04/29 17:00:15 by varandri            #+#    #+#            #
-#   Updated: 2026/06/10 16:51:10 by varandri           ###   ########.fr      #
+#   Updated: 2026/06/12 23:52:34 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
-from app import read_file, MazeDisplayer
-
+from app import a_maze_ing
 import sys
 
-
 if __name__ == "__main__":
-    _, file = sys.argv
-    test = read_file(file)
-    cell_size = 0
-    if test["width"] >= test["height"]:
-        cell_size = 1000 // test["width"]
-    else:
-        cell_size = 1000 // test["height"]
-    wall_size = max(1, cell_size // 8)
-    win_width = test["width"] * cell_size
-    win_height = test["height"] * cell_size
     try:
-        displayer: MazeDisplayer = MazeDisplayer(
-            test, win_width, win_height, cell_size, wall_size
-        )
+        _, file = sys.argv
+        a_maze_ing(file)
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
