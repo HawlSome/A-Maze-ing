1aceae231601ed918d6f8851480e3196a6796b26
varandri <varandri@student.42antananarivo.mg>
2026-06-14 17:01:57 +0300

    all - done && clean && no lint

--- app/gui/ft_mlx/__init__.py
+++ app/gui/ft_mlx/__init__.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # ########################################################################### #
-#   shebang: 1                                                                #
+#                                                                             #
 #                                                          :::      ::::::::  #
 #   __init__.py                                          :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
 #   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/05/29 14:49:36 by varandri            #+#    #+#            #
-#   Updated: 2026/06/13 00:11:27 by varandri           ###   ########.fr      #
+#   Updated: 2026/06/14 17:01:31 by varandri           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
-from .main import Mlx, PyMlx, c_void_p
+from mlx import Mlx, c_void_p
+from .main import PyMlx
 
-__all__ = ["Mlx", "PyMlx", "c_void_p"]
+__all__ = ["PyMlx", "Mlx", "c_void_p"]
